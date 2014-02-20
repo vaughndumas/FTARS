b1f60ab874aff9852258fd59fb9a10b41817b7eb
Vaughn Dumas <vaughndumas@gmail.com>
2014-02-20 16:36:25 +1100

    Setup v3
    Use the correct field names.

--- js/db_faa.js
+++ js/db_faa.js
@@ -3,11 +3,17 @@
 /////
 function getFaa() {
     db.transaction(function(tx) {
-       tx.executeSql('SELECT * FROM faaftp WHERE ROWNUM',
+       tx.executeSql('SELECT * FROM faaftp',
                      [],
                      function(tx, v_results) {
                          v_item = document.getElementById("x_faasdate");
                          v_item.textContent = v_results.rows.item(0).faasdate;
+                         v_item = document.getElementById("x_faaedate");
+                         v_item.textContent = v_results.rows.item(0).faaedate;
+                         v_item = document.getElementById("x_faadesc");
+                         v_item.textContent = v_results.rows.item(0).faadesc;
+                         v_item = document.getElementById("x_faaarea");
+                         v_item.textContent = v_results.rows.item(0).faaarea;
                      },
                      errorHandler);
     });
